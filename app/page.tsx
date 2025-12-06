48585fbfd6eb406cf4d3f04bfe70610288e26d07
viniciocoelho <coelhocvcg@gmail.com>
2025-12-05 22:50:16 -0300

    Alterações iniciais do projeto
    Configuração da tela inicial separando a área do texto da seção onde serão apresentadas as fotos.
    Criado o grid layout para as imagens.
    Criado o tipo Photo definindo id e url.
    Criada a lista padrão com as 9 imagens que serão apresentadas na galeria.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 "use client"
 
+import { photoList } from "./data/photoList";
+
 const Page = () => {
 
  
   return (
 
-    <div>XXX</div>
-  
+    <div className="mx-2">
+      <h1 className="text-center text-3xl font-bold my-10">Fotos Intergalaticas</h1>
+
+      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {photoList.map(item => (
+          <div> XXX </div>
+        ))}
+
+      </section>
+
+    </div> 
     
   );
 }
