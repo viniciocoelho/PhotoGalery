bbb0d4a8bd6f03193818068d2ea99cd0978121c4
viniciocoelho <coelhocvcg@gmail.com>
2025-12-07 19:17:10 -0300

    Novos Componentes
    Criados novos componente Modal e PhotoItem.
    Feita a implementação dos componentes no Page.tsx para exibir a lista das imagens.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
 "use client"
 
+import { useState } from "react";
+import { PhotoItem } from "./components/PhotoItem";
 import { photoList } from "./data/photoList";
+import { Modal } from "./components/Modal";
 
 const Page = () => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const [imageOfModal, setImageOfModal] = useState('');
+
+  const openModal = (id: number) => {
+    const photo = photoList.find(item => item.id === id);
+    if(photo) {
+      setImageOfModal(photo.url);
+      setShowModal(true);
+    }
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
 
  
   return (
@@ -12,10 +31,18 @@ const Page = () => {
 
       <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {photoList.map(item => (
-          <div> XXX </div>
+          <PhotoItem 
+            key={item.id}
+            photo={item}
+            onClick={() => openModal(item.id)}
+          />
         ))}
 
       </section>
+
+      {showModal &&
+        <Modal image={imageOfModal} closeModal={closeModal}/>
+      }
 
     </div> 
     
