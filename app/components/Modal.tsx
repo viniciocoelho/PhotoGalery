ddfcd7f4bca11f5f9067ec4305c77c842a49a6d9
viniciocoelho <coelhocvcg@gmail.com>
2025-12-07 19:29:58 -0300

    Projeto finalizado
    Criado o componente Modal, que aumenta o tamanho da imagem clicada e depois fecha, também clicando, e voltando para a tela inicial do grip.
    Componente incluído no showModal.

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,18 @@
-export const Modal = () => {
+type Props = {
+    image: string;
+    closeModal: () => void;
+}
+
+export const Modal = ({image, closeModal}: Props) => {
     return (
-        <div> XXX </div>
+        <>
+            <div onClick={closeModal} className="fixed left-0 right-0 top-0 bottom-0 flex justify-center items-center bg-black/90 ">
+                <img src={`/assets/${image}`} alt="" className="max-w-screen max-h-screen"/>
+            </div>
+            <div onClick={closeModal} className="fixed top-5 right-5 w-10 h-10 cursor-pointer text-5xl">
+                X
+            </div>
+        
+        </>
     );
 }
